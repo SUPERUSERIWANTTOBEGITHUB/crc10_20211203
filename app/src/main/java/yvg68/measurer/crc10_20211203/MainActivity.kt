f56d4ce8ba75b09cbfc557256935c75efdf4a5c5
AS <yvg68@yandex.ru>
2021-12-08 19:56:45 +0300

    created Logs MainActivity

--- app/src/main/java/yvg68/measurer/crc10_20211203/MainActivity.kt
+++ app/src/main/java/yvg68/measurer/crc10_20211203/MainActivity.kt
@@ -2,19 +2,45 @@ package yvg68.measurer.crc10_20211203
 //
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
+private const val TAG = "MyTAG"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // connect fragment by activity
+        Log.d(TAG, "MyActivity_onCreate")
+
+        // connecting fragment by activity
         val startFragment = StartFragment()
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.fragment_container, startFragment)
             .commit()
+    }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"MyActivity_onStart")
+    }
 
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"MyActivity_onResume")
+    }
 
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"MyActivity_onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"MyActivity_onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"MyActivity_onDestroy")
     }
 }
