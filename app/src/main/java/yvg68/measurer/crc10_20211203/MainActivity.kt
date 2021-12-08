9c03239ef251196fd266b9fd452153eb131e1864
AS <yvg68@yandex.ru>
2021-12-08 18:08:58 +0300

    created one fragment

--- app/src/main/java/yvg68/measurer/crc10_20211203/MainActivity.kt
+++ app/src/main/java/yvg68/measurer/crc10_20211203/MainActivity.kt
@@ -7,7 +7,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // connect fragment by activity
         val startFragment = StartFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, startFragment)
+            .commit()
+
+
 
     }
 }
