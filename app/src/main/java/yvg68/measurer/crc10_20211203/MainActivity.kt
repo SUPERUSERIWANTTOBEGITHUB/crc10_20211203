6443c02c5b8f05f7c4580a2500427faf7b51f26e
AS <yvg68@yandex.ru>
2021-12-08 17:49:08 +0300

    init

---
+++ app/src/main/java/yvg68/measurer/crc10_20211203/MainActivity.kt
@@ -0,0 +1,13 @@
+package yvg68.measurer.crc10_20211203
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val startFragment = StartFragment()
+
+    }
+}
---
+++ app/src/main/java/yvg68/measurer/crc10_20211203/StartFragment.kt
@@ -0,0 +1,38 @@
+package yvg68.measurer.crc10_20211203
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [StartFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class StartFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_start, container, false)
+
+        //some work
+         return view
+        
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+
+
+// 1 выносим вью в отдельную переменную
+// 2 совершаем работу с вью
+// 3 возвращаем вью
+}
